370866f5e23d8408950d54072630a18b2ba73f63
dopple <kuberin_b@naver.com>
2021-08-07 19:23:04 +0900

    2D 종스크롤 슈팅 - 텍스트파일을 이용한 커스텀 배치 구현 [유니티 기초 강좌 B35]

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -8,13 +10,12 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Inst { get; private set; }
-    void Awake() => Inst = this;
 
     //---------------------------------------------------
     public Transform[] spawnPoints;
 
-    public float maxSpawnDelay;
-    public float curSpawnDelay;
+    float nextSpawnDelay;
+    float curSpawnDelay;
     //---------------------------------------------------
     public Player player;
     //---------------------------------------------------
@@ -27,59 +28,58 @@ public class GameManager : MonoBehaviour
     //---------------------------------------------------
 
 
-    public int score;
-    int playerLife = 3;
+    [HideInInspector] public int score;
+    static int MAX_PLAYER_LIFE = 3;
+    int playerLife = MAX_PLAYER_LIFE;
 
-
-
-
-    void Start()
+    void Awake()
     {
+        Inst = this;
+
+        ReadSpawnFile();
+
         UpdateLifeIcon();
         UpdateBombIcon();
     }
 
-    private void Update()
+    void Update()
     {
-        scoreText.text = score.ToString("#,##0");
-
         curSpawnDelay += Time.deltaTime;
 
-        if (curSpawnDelay > maxSpawnDelay)
+        if (curSpawnDelay > nextSpawnDelay && spawnEnd == false )
         {
             SpawnEnemy();
-
-            maxSpawnDelay = Random.Range(0.5f, 3.0f);
             curSpawnDelay = 0.0f;
         }
+
+        scoreText.text = score.ToString("#,##0");
     }
 
     void SpawnEnemy()
     {
-        int randEnemy = Random.Range(0, 3);
-        int randPoint = Random.Range(0, spawnPoints.Length);
-
-        PoolType poolType;
-        if ( randEnemy == 0 )
+        PoolType poolType = PoolType.enemyS;
+        if (spawnList[spawnIndex].type == "L" )
             poolType = PoolType.enemyL;
-        else if(randEnemy == 1)
+        else if (spawnList[spawnIndex].type == "M")
             poolType = PoolType.enemyM;
-        else
+        else if (spawnList[spawnIndex].type == "S")
             poolType = PoolType.enemyS;
 
+        int enemyPoint = spawnList[spawnIndex].point;
+
         var enemyObj = ObjectManager.Inst.MakeObj(poolType);
-        enemyObj.transform.position = spawnPoints[randPoint].position;
-        enemyObj.transform.rotation = spawnPoints[randPoint].rotation;
+        enemyObj.transform.position = spawnPoints[enemyPoint].position;
+        enemyObj.transform.rotation = spawnPoints[enemyPoint].rotation;
 
         var rigid = enemyObj.GetComponent<Rigidbody2D>();
         var enemy = enemyObj.GetComponent<Enemy>();
 
-        if (randPoint == 5 || randPoint == 6)
+        if (enemyPoint == 5 || enemyPoint == 6)
         {
             enemyObj.transform.Rotate(Vector3.back * 90.0f);
             rigid.velocity = new Vector2(enemy.speed * -1.0f, -1.0f);
         }
-        else if (randPoint == 7 || randPoint == 8)
+        else if (enemyPoint == 7 || enemyPoint == 8)
         {
             enemyObj.transform.Rotate(Vector3.forward * 90.0f);
             rigid.velocity = new Vector2(enemy.speed, -1.0f);
@@ -88,6 +88,17 @@ public class GameManager : MonoBehaviour
         {
             rigid.velocity = new Vector2(0.0f, enemy.speed * -1.0f);
         }
+
+        // #.리스폰 인덱스 증가
+        spawnIndex++;
+        if (spawnIndex == spawnList.Count)
+        {
+            spawnEnd = true;
+            return;
+        }
+
+        // #.다음 리스폰 딜레이 갱신
+        nextSpawnDelay = spawnList[spawnIndex].delay;
     }
 
     public void OnClick_Retry()
@@ -210,4 +221,48 @@ public class GameManager : MonoBehaviour
             bombImage[i].color = Color.white;
         }
     }
+
+
+
+    List<Spawn> spawnList = new List<Spawn>();
+    int spawnIndex;
+    bool spawnEnd;
+
+    void ReadSpawnFile()
+    {
+        // #1.변수 초기화
+        spawnList.Clear();
+        spawnIndex = 0;
+        spawnEnd = false;
+
+        // #2.리스폰 파일 읽기
+        var textFile = Resources.Load("Stage0") as TextAsset;
+        var reader = new StringReader(textFile.text);
+        // #. 첫 줄은 넘긴다
+        reader.ReadLine();
+
+        // #3. 한줄씩 데이터 저장
+        while (reader != null)
+        {
+            string line = reader.ReadLine();
+
+            if (line == null)
+                break;
+
+            var data = new Spawn();
+
+            string[] v = line.Split(',');
+            data.delay = float.Parse(v[0]);
+            data.type = v[1];
+            data.point = int.Parse(v[2]);
+
+            spawnList.Add(data);
+        }
+        // #4.텍스트 파일 닫기
+        reader.Close();
+
+        // #. 첫번쨰 스폰 딜레이
+        nextSpawnDelay = spawnList[0].delay;
+    }
+
 }
